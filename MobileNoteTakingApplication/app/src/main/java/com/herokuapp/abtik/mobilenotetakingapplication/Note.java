8d72a398efdaea949522f5e596ac8473a4e8c663
Edison CHAN <chanw30@student.douglascollege.ca>
2023-11-24 14:48:43 -0800

    Feat: Update Note object to contain 3 new data, tag (for implementation with color), due date, last edit

--- MobileNoteTakingApplication/app/src/main/java/com/herokuapp/abtik/mobilenotetakingapplication/Note.java
+++ MobileNoteTakingApplication/app/src/main/java/com/herokuapp/abtik/mobilenotetakingapplication/Note.java
@@ -3,6 +3,8 @@ package com.herokuapp.abtik.mobilenotetakingapplication;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
+import java.util.Date;
+
 @Entity(tableName = "note_table")
 public class Note {
 
@@ -12,6 +14,16 @@ public class Note {
     public String title;
     public String description;
 
+    public Date dueDate;
+
+    public Date lastEdit;
+
+    public String tag;
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public Note(String title, String description) {
         this.title = title;
         this.description = description;
@@ -19,6 +31,46 @@ public class Note {
 
     public int getId() {
         return id;
+    }
+
+    public Note() {
+    }
+
+    public Note(int id, String title, String description, Date dueDate, Date lastEdit, String tag) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.lastEdit = lastEdit;
+        this.tag = tag;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dueDate) {
+        this.dueDate = dueDate;
+    }
+
+    public Date getLastEdit() {
+        return lastEdit;
+    }
+
+    public void setLastEdit(Date lastEdit) {
+        this.lastEdit = lastEdit;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
     }
 
     public String getTitle() {
