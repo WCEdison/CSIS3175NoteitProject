ce6e05489b82a89cf20c969326ce311395f9f4e9
Edison CHAN <chanw30@student.douglascollege.ca>
2023-12-01 01:08:21 -0800

    Feat: Adde Temp Notification SYstem

--- app/src/main/java/com/example/noteitapp/MainActivity.java
+++ app/src/main/java/com/example/noteitapp/MainActivity.java
@@ -1,13 +1,19 @@
 package com.example.noteitapp;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.MenuItem;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -16,6 +22,7 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String CHANNEL_ID = "NOTEIT";
     private ListView notesListView;
     private NoteAdapter noteAdapter;
     private NoteDatabaseHelper db;
@@ -29,10 +36,10 @@ public class MainActivity extends AppCompatActivity {
         db = new NoteDatabaseHelper(this);
         notesListView = findViewById(R.id.notes_list_view);
         ArrayList<Note> notes = db.getAllNotes();
-        Toast.makeText(MainActivity.this, "Welcome, you have: " + notes.size() + " events." , Toast.LENGTH_SHORT).show();
-
+        Toast.makeText(MainActivity.this, "Welcome, you have: " + notes.size() + " events.", Toast.LENGTH_SHORT).show();
+        TriggerNotification("Note it update!", notes.size() + " events.");
         updateUI();
-
+        createNotificationChannel();
         notesListView.setOnItemClickListener((parent, view, position, id) -> {
             Note note = (Note) parent.getItemAtPosition(position);
             Intent intent = new Intent(MainActivity.this, NoteActivity.class);
@@ -44,6 +51,13 @@ public class MainActivity extends AppCompatActivity {
         setBottomNav();
     }
 
+//    void TriggerNotification(){
+//        Intent notificationintent = new Intent(Settings.ACTION_CHANNEL_NOTIFICATION_SETTINGS);
+//        notificationintent.putExtra(Settings.EXTRA_APP_PACKAGE, getPackageName());
+//        notificationintent.putExtra(Settings.EXTRA_CHANNEL_ID, CHANNEL_ID);
+//        startActivity(notificationintent);
+//    }
+
     @Override
     protected void onResume() {
         super.onResume();
@@ -52,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void updateUI() {
         ArrayList<Note> notes = db.getAllNotes();
-       // Toast.makeText(MainActivity.this, "Welcome, you have: " + notes.size() + " events." , Toast.LENGTH_SHORT).show();
+        // Toast.makeText(MainActivity.this, "Welcome, you have: " + notes.size() + " events." , Toast.LENGTH_SHORT).show();
         if (noteAdapter == null) {
             noteAdapter = new NoteAdapter(this, notes);
             notesListView.setAdapter(noteAdapter);
@@ -88,5 +102,35 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is not in the Support Library.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = getString(R.string.channel_name);
+            String description = getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this.
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+    }
 
+    void TriggerNotification(String title, String content) {
+        // Create an explicit intent for an Activity in your app.
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.notification_icon)
+                .setContentTitle(title)
+                .setContentText(content)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                // Set the intent that fires when the user taps the notification.
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+    }
 }
