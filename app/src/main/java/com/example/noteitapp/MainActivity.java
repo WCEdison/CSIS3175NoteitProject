763c660df4df1586828d1c26fb5f2963ded5f1b7
Edison CHAN <chanw30@student.douglascollege.ca>
2023-12-01 01:18:32 -0800

    Feat: Added Notification System

--- app/src/main/java/com/example/noteitapp/MainActivity.java
+++ app/src/main/java/com/example/noteitapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
@@ -13,12 +14,16 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -38,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         ArrayList<Note> notes = db.getAllNotes();
         Toast.makeText(MainActivity.this, "Welcome, you have: " + notes.size() + " events.", Toast.LENGTH_SHORT).show();
         TriggerNotification("Note it update!", notes.size() + " events.");
+
         updateUI();
         createNotificationChannel();
         notesListView.setOnItemClickListener((parent, view, position, id) -> {
@@ -132,5 +138,21 @@ public class MainActivity extends AppCompatActivity {
                 // Set the intent that fires when the user taps the notification.
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+        // notificationId is a unique int for each notification that you must define.
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        int id = (int) Calendar.getInstance().getTime().getTime();
+        notificationManager.notify(id, builder.build());
     }
 }
