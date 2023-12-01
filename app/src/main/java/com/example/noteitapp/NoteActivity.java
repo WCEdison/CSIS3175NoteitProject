eeb5aa91e9204b001a107924a2052c5a246537c8
Edison CHAN <chanw30@student.douglascollege.ca>
2023-11-30 21:59:48 -0800

    Feat: Added Date picker for add and view note

--- app/src/main/java/com/example/noteitapp/NoteActivity.java
+++ app/src/main/java/com/example/noteitapp/NoteActivity.java
@@ -1,7 +1,11 @@
 package com.example.noteitapp;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -10,7 +14,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.Calendar;
+
 public class NoteActivity extends AppCompatActivity {
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton;
     private TextView titleTextView;
     private TextView descriptionTextView;
     private NoteDatabaseHelper db;
@@ -24,7 +32,9 @@ public class NoteActivity extends AppCompatActivity {
         db = new NoteDatabaseHelper(this);
         titleTextView = findViewById(R.id.title_text_view);
         descriptionTextView = findViewById(R.id.description_text_view);
-
+        initDatePicker();
+        dateButton = findViewById(R.id.datePickerButton);
+        dateButton.setText(getTodaysDate());
         int noteId = getIntent().getIntExtra("NOTE_ID", -1);
         if (noteId != -1) {
             note = db.getNote(noteId);
@@ -48,7 +58,7 @@ public class NoteActivity extends AppCompatActivity {
                 EditText descriptionEditText = findViewById(R.id.description_text_view);
                 String title = titleEditText.getText().toString();
                 String description = descriptionEditText.getText().toString();
-
+                String duedate = dateButton.getText().toString();
                 Note newNote = new Note(note.getId(), title, description);
                 db.updateNote(newNote);
                 Toast.makeText(NoteActivity.this, "Note updated", Toast.LENGTH_SHORT).show();
@@ -56,4 +66,81 @@ public class NoteActivity extends AppCompatActivity {
             }
         });
     }
+
+    private String getTodaysDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    private void initDatePicker()
+    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(this, style, dateSetListener, year, month, day);
+        //datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+    }
+
+    private String makeDateString(int day, int month, int year)
+    {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month)
+    {
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+        //default should never happen
+        return "JAN";
+    }
+
+    public void openDatePicker(View view)
+    {
+        datePickerDialog.show();
+    }
+
 }
